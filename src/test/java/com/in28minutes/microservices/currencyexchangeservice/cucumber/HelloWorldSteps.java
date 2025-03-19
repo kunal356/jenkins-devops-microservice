cf252ff1579ef08f7791d103e00d782608cb41fb
Kunal Gaikwad <gaikwadkunal610@gmail.com>
2025-03-19 19:14:27 +0000

    Update Test code.

--- src/test/java/com/in28minutes/microservices/currencyexchangeservice/cucumber/HelloWorldSteps.java
+++ src/test/java/com/in28minutes/microservices/currencyexchangeservice/cucumber/HelloWorldSteps.java
@@ -1,8 +1,8 @@
 package com.in28minutes.microservices.currencyexchangeservice.cucumber;
 
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.junit.Assert;
 
 import com.in28minutes.microservices.currencyexchangeservice.HelloWorld;
@@ -10,7 +10,6 @@ import com.in28minutes.microservices.currencyexchangeservice.HelloWorld;
 public class HelloWorldSteps {
 
     private HelloWorld helloWorld = new HelloWorld();
-
 
     private String name = "";
 
@@ -20,15 +19,16 @@ public class HelloWorldSteps {
     public void givenInput(String name) {
         this.name = name;
     }
+
     @When("^sayHello method of HelloWorld.java is called$")
     public void whenBusinessLogicCalled() {
         output = helloWorld.sayHello(name);
     }
+
     @Then("^It should return (.*)$")
     public void thenCheckOutput(String response) {
         Assert.assertEquals(output, response);
     }
-
 
     public static void main(String[] args) {
     }
