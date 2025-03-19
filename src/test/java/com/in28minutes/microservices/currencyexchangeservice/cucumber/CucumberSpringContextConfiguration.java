1cf901b3d8c1289c38c299ac4bc894012f6a789e
Kunal Gaikwad <gaikwadkunal610@gmail.com>
2025-03-19 22:25:28 +0000

    Restore to in28min version

--- src/test/java/com/in28minutes/microservices/currencyexchangeservice/cucumber/CucumberSpringContextConfiguration.java
+++ src/test/java/com/in28minutes/microservices/currencyexchangeservice/cucumber/CucumberSpringContextConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.ContextConfiguration;
 
 import com.in28minutes.microservices.currencyexchangeservice.CurrencyExchangeServiceApplicationH2;
 
-import io.cucumber.java.Before;
+import cucumber.api.java.Before;
 
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 @ContextConfiguration(classes = CurrencyExchangeServiceApplicationH2.class, loader = SpringBootContextLoader.class)
